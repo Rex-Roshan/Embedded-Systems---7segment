50d3b883b861c3a035f26b85edc0cf73da3789c9
Rex-Roshan <95865657+Rex-Roshan@users.noreply.github.com>
2024-01-17 20:43:16 +0530

    Add files via upload
    One code displays the temperature detected by the temparature sensor

---
+++ 7seg/7seg.ino
@@ -0,0 +1,135 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,1);
+  digitalWrite(8,0);
+  digitalWrite(9,1);
+
+  delay(1000);
+
+  digitalWrite(2,0);13
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,0);
+  digitalWrite(7,0);
+  digitalWrite(8,0);
+  digitalWrite(9,0);
+  
+  delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,0);
+  digitalWrite(6,1);
+  digitalWrite(7,1);
+  digitalWrite(8,1);
+  digitalWrite(9,0);
+
+  delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+  digitalWrite(9,0);
+  
+  delay(1000);
+
+  digitalWrite(2,0);
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,0);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+  digitalWrite(9,1);
+
+    delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,0);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+  digitalWrite(9,1);
+
+  
+    delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,0);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,1);
+  digitalWrite(8,1);
+  digitalWrite(9,1);
+
+  
+    delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  digitalWrite(6,0);
+  digitalWrite(7,0);
+  digitalWrite(8,0);
+  digitalWrite(9,0);
+
+  
+    delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,1);
+  digitalWrite(8,1);
+  digitalWrite(9,1);
+
+  
+    delay(1000);
+
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+  digitalWrite(9,1);
+
+
+
+  
+  delay(1000);
+}
+//}iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllp
+
+//ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
